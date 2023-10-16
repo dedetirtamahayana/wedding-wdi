0c713e70c49333e5c98e01f096daa1549596e226
Dede Mahayana <dedetirtamahayana@gmail.com>
2023-10-16 21:27:31 +0800

    change bg-color mobile backdrop

--- src/components/Backdrop/Backdrop.jsx
+++ src/components/Backdrop/Backdrop.jsx
@@ -6,16 +6,6 @@ const Backdrop = ({ onClick }) => {
   const { undangan } = router.query;
   return (
     <div className='fixed top-0 left-0 w-full h-full flex flex-col items-center justify-center'>
-      <div
-        className='fixed top-0 left-0 w-full h-full'
-        style={{
-          backgroundImage: `url('/bg1.png')`, // Ganti dengan path gambar yang benar
-          backgroundSize: "20% auto",
-          backgroundColor: "#181818",
-          backgroundRepeat: "repeat", // Menambahkan efek blur hanya pada latar belakang
-        }}
-      ></div>
-      <div className='bg-black bg-opacity-0 lg:bg-opacity-50 absolute inset-0 '></div>
       <div className=' text-white z-10 text-center mb-4'>
         <img src='/wedding.jpg' alt='img-circle' className='w-60 mx-auto' />
         <h1 className='text-3xl custom-text mb-4 text-gold pt-5'>
