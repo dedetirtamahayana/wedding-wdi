6772acf0064a2674a31dbe93136ecacd4e9bb4c7
Dede Mahayana <dedetirtamahayana@gmail.com>
2023-10-16 21:22:35 +0800

    edit mobile backgrop

--- src/components/Backdrop/Backdrop.jsx
+++ src/components/Backdrop/Backdrop.jsx
@@ -15,11 +15,11 @@ const Backdrop = ({ onClick }) => {
           backgroundRepeat: "repeat", // Menambahkan efek blur hanya pada latar belakang
         }}
       ></div>
-      <div className='bg-black bg-opacity-50 absolute inset-0 '></div>
+      <div className='bg-black bg-opacity-0 lg:bg-opacity-50 absolute inset-0 '></div>
       <div className=' text-white z-10 text-center mb-4'>
         <img src='/wedding.jpg' alt='img-circle' className='w-60 mx-auto' />
-        <h1 className='text-7xl custom-text mb-4 text-gold'>
-          Komodo & Sarizki
+        <h1 className='text-3xl custom-text mb-4 text-gold pt-5'>
+          Wahyudi & Vina
         </h1>
         <p>Undangan Pawiwahan</p>
         <p>Kepada Yth. Bapak/Ibu/Saudara/i</p>
